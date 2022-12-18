823e3f1746abdf7e454a0153e765235c5e1860f6
laurafcamargos <laurafernandes1465@gmail.com>
2022-12-18 02:05:21 -0300

    atualizando

--- t2_contatinhos/contatinho.h
+++ t2_contatinhos/contatinho.h
@@ -2,12 +2,11 @@
 #define CONTATINHO_H
 
 #define JA_EXISTE -2
-#define POSICAO_INVALIDA -1
 #define ERRO_CHEIA 0
 #define SUCESSO 1
 
 //estrutura do identificador do elemento
-typedef char t_chave[12];
+typedef char t_chave[50];
 
 //estrutura do tipo elemento
 typedef struct {
@@ -34,11 +33,10 @@ typedef t_apontador t_avl;
 void criar(t_avl *avl);//cria a árvore
 int retornar_altura(t_avl *avl);
 int checar_fb(t_avl *avl);
-
-int inserir(t_avl *avl, t_elemento elemento);//insere novos contatos
-int pesquisar(t_avl *avl, t_chave nome);//devolve a posição buscada
-int remover(t_avl *avl, t_chave nome);//remove contatos já existentes
-int alterar(t_avl *avl, t_elemento novo);//altera o contato encontrado
+int inserir(t_avl *avl, t_elemento elemento, int *idxResp, int respostas[100000]);//insere novos contatos
+int pesquisar(t_avl *avl, t_chave nome, int *idxResp, int respostas[100000], int *idxTel, long int telefones[100000]);//devolve a posição buscada
+int remover(t_avl *avl, t_chave nome, int *idxResp, int respostas[100000]);//remove contatos já existentes
+int alterar(t_avl *avl, t_elemento novo, int *idxResp, int respostas[100000]);//altera o contato encontrado
 void ApagaArvore(t_avl *avl);//libera a memória, apagando os nós da árvore
 int ComparaStrings(char *str, char *ing);//compara as strings char a char, auxiliando a pesquisa dos contatos
 #endif
